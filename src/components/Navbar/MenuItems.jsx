4ba1b613758e1b2f6ddaf92147047e3ebe060af4
Jabez Roberts <info@jabezroberts.com>
2022-08-02 09:25:50 -0500

    first and only commit of completed project

---
+++ src/components/Navbar/MenuItems.jsx
@@ -0,0 +1,33 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Poducts',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Sign-Up',
+        url: '#',
+        cName: 'nav-links-mobile',
+    },
+
+]
